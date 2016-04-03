26b8c4e7f7a1d49494f4221662d69412e2183fe1
Calvin Webster <calweb@gmail.com>
2016-04-03 19:25:45 -0400

    remove comments

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-
-// proj4.defs("EPSG:32033","+proj=lcc +lat_1=32.33333333333334 +lat_2=33.66666666666666 +lat_0=31.83333333333333 +lon_0=-81 +x_0=609601.2192024384 +y_0=0 +datum=NAD27 +units=us-ft +no_defs");
-
-// gmaps api key: AIzaSyBCAYCXrMRZCXrevgJzhWcLZKYBze2GaBg
-
-var coords;
 var map;
 
 (function() {
